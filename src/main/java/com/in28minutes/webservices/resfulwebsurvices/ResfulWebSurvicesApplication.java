2168e1c4e94a4b3749831e4b690f3b6a4679b7e2
Askell <fafapo768@gmail.com>
2024-07-20 18:06:00 +1000

    Initial commit

---
+++ src/main/java/com/in28minutes/webservices/resfulwebsurvices/ResfulWebSurvicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.webservices.resfulwebsurvices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulWebSurvicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResfulWebSurvicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/in28minutes/webservices/resfulwebsurvices/ResfulWebSurvicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.webservices.resfulwebsurvices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulWebSurvicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
